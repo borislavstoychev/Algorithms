07d53c9cde5a872c2c568d0d46f3b4b89cb4e7c3
borislavstoychev <stoy4ew@gmail.com>
2022-08-03 11:58:56 +0300

    Kruskal’s Algorithm and Prim's Algorithm

---
+++ Graphs–Shortest Path and MST/lab/kruskal_algorithm.py
@@ -0,0 +1,48 @@
+class Edge():
+  def __init__(self, first, second, weight) -> None:
+    self.first = first
+    self.second = second
+    self.weight = weight
+
+class Graph():
+  
+  def __init__(self, edges, graph, max_node) -> None:
+    self.edges = edges
+    self.graph = graph
+    self.max_node = max_node
+    self.forest = []
+    self.parent = [num for num in range(max_node + 1)]
+    
+  def find_root(self, node):
+    while node != self.parent[node]:
+      node = self.parent[node]
+    return node
+
+  def kruskal(self):
+    for edge in sorted(self.graph, key=lambda e: e.weight):
+      first_node_root = self.find_root(edge.first)
+      second_node_root = self.find_root(edge.second)
+      if first_node_root != second_node_root:
+        self.parent[first_node_root] = second_node_root
+        self.forest.append(edge)
+
+  def print_path(self):
+    for edge in self.forest:
+      print(f"{edge.first} - {edge.second}")
+
+
+def main():
+  edges = int(input())
+  graph = []
+  max_node = float('-inf')
+  for _ in range(edges):
+    first, second, weight = [int(n) for n in input().split(', ')]
+    graph.append(Edge(first, second, weight))
+    max_node = max(first, second, max_node)
+
+  solution = Graph(edges, graph, max_node)
+  solution.kruskal()
+  solution.print_path()
+
+if __name__ == "__main__":
+  main()
---
+++ Graphs–Shortest Path and MST/lab/prim_algorithm.py
@@ -0,0 +1,67 @@
+from collections import deque
+from queue import PriorityQueue
+
+class Edge():
+  def __init__(self, first, second, weight) -> None:
+    self.first = first
+    self.second = second
+    self.weight = weight
+
+  def __gt__(self, other):
+    return self.weight > other.weight
+
+class Graph():
+  
+  def __init__(self, edges, graph) -> None:
+    self.edges = edges
+    self.graph = graph
+    self.forest = set()
+    self.forest_edges = []
+    self.path = deque()
+    self.pq = PriorityQueue()
+
+  def prim(self, node):
+    self.forest.add(node)
+    for edge in self.graph[node]:
+      self.pq.put(edge)
+    while not self.pq.empty():
+      min_edge = self.pq.get()
+      non_tree_node = -1
+      if min_edge.first in self.forest and min_edge.second not in self.forest:
+        non_tree_node = min_edge.second
+      elif min_edge.second in self.forest and min_edge.first not in self.forest:
+        non_tree_node = min_edge.first
+      if non_tree_node == -1:
+        continue
+      self.forest.add(non_tree_node)
+      self.forest_edges.append(min_edge)
+      for edge in self.graph[non_tree_node]:
+        self.pq.put(edge)
+
+  def print_path(self):
+    for node in self.graph:
+      if node in self.forest:
+        continue
+      self.prim(node)
+    
+    for edge in self.forest_edges:
+      print(f"{edge.first} - {edge.second}")
+
+
+def main():
+  edges = int(input())
+  graph = {}
+  for _ in range(edges):
+      first, second, weight = [int(n) for n in input().split(', ')]
+      if first not in graph:
+        graph[first] = []
+      if second not in graph:
+        graph[second] = []
+      graph[first].append(Edge(first, second, weight))
+      graph[second].append(Edge(first, second, weight))
+
+  solution = Graph(edges, graph)
+  solution.print_path()
+
+if __name__ == "__main__":
+  main()
